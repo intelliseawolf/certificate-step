3f044a31caeb377e88c4b69f8a4919b1e4f1443a
ienjoyjs <ust.organ@gmail.com>
2022-09-18 02:40:24 +0200

    fixed: delete texts

--- resources/js/src/components/certificate/Modal/UploadImageModal.vue
+++ resources/js/src/components/certificate/Modal/UploadImageModal.vue
@@ -1,32 +1,22 @@
 <template>
   <div class="upload-image-modal">
-    <vs-prompt
-      @close="close"
-      @accept="acceptAlert"
-      buttonAccept="false"
-      buttonCancel="false"
-      :active="activePrompt"
-      title=""
-    >
+    <vs-prompt @close="close" @accept="acceptAlert" buttonAccept="false" buttonCancel="false"
+      :active.sync="activePrompt" title="">
       <h3 class="text-center mb-4">Image Upload</h3>
       <div class="con-exemple-prompt">
-        <certificate-file-upload
-          class="editor-image-upload"
-          :file_repository_id="4103"
-          section_type="CertificateComponent"
-          @image="setImagePath"
-        />
-        <div class="flex mt-3">
+        <certificate-file-upload class="editor-image-upload" :file_repository_id="4103"
+          section_type="CertificateComponent" @image="setImagePath" />
+        <!-- <div class="flex mt-3">
           <vs-button class="ml-auto mr-3" @click="toggleModal">Cancel</vs-button>
           <vs-button @click="handleOk">Ok</vs-button>
-        </div>
+        </div> -->
       </div>
     </vs-prompt>
   </div>
 </template>
 
 <script>
-import CertificateFileUpload from '../editor/CertificateFileUpload.vue';
+import CertificateFileUpload from '../editor/CertificateFileUpload.vue'
 
 export default {
   name: "UploadImageModal",
@@ -57,14 +47,16 @@ export default {
     },
     setImagePath(image) {
       this.image = image.file.file_path
+      this.activePrompt = false
+      this.$emit("handleOk", this.image)
     },
     toggleModal() {
       this.activePrompt = !this.activePrompt
     },
-    handleOk() {
-      this.activePrompt = false
-      this.$emit("handleOk", this.image)
-    }
+    // handleOk() {
+    //   this.activePrompt = false
+    //   this.$emit("handleOk", this.image)
+    // }
   }
 }
 </script>
